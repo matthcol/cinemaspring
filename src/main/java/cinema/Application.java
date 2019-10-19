2b80acb6d9aae14e2215156d8e4a68591f8f29aa
Matthias Colin <matthias.colin@gmail.com>
2019-10-19 23:03:37 +0200

    adapt pom.xml and Application.java to be a webapp

--- src/main/java/cinema/Application.java
+++ src/main/java/cinema/Application.java
@@ -1,11 +1,9 @@
 package cinema;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+public class Application extends SpringBootServletInitializer {
+	
 }
